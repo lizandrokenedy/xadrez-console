fbd1e7a200e81dd486e8f51847f39eae4af8ebe2
Lizandro Kenedy <lizandrosantos@live.com>
2024-05-24 21:23:35 -0300

    Adding class position

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console { 
+    internal class Program {
+        static void Main(string[] args) {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
