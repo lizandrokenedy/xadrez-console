64169c63178d965c58fe77b04f055f73cf274c84
Lizandro Kenedy <lizandrosantos@live.com>
2024-05-25 21:43:28 -0300

    Adding treatment errors

--- Program.cs
+++ Program.cs
@@ -5,16 +5,24 @@ using xadrez;
 namespace xadrez_console {
     internal class Program {
         static void Main(string[] args) {
-            Tabuleiro tab = new Tabuleiro(8, 8);
 
-             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-             tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            //tab.colocarPeca(???, new Posicao(0, 0));
-            Tela.imprimirTabuleiro(tab);
+                //tab.colocarPeca(???, new Posicao(0, 0));
+                Tela.imprimirTabuleiro(tab);
+
+
+            }
+            catch (TabuleiroException ex) {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadLine();
+
         }
     }
 }
