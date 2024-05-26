eb0cc18a7f41cf3022ce4b41d679c3a22dca6c89
Lizandro Kenedy <lizandrosantos@live.com>
2024-05-26 14:43:34 -0300

    Adding class position chess

--- Program.cs
+++ Program.cs
@@ -7,13 +7,21 @@ namespace xadrez_console {
         static void Main(string[] args) {
 
             try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                //Tabuleiro tab = new Tabuleiro(8, 8);
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                //tab.colocarPeca(???, new Posicao(0, 0));
-                Tela.imprimirTabuleiro(tab);
+                ////tab.colocarPeca(???, new Posicao(0, 0));
+                //Tela.imprimirTabuleiro(tab);
+
+                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+                Console.WriteLine(pos);
+
+                Console.WriteLine(pos.toPosicao());
+
+                Console.ReadLine();
 
 
             }
