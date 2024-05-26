1f1ab79687ee7b7fc82e04cb26c388c0da3b5a28
Lizandro Kenedy <lizandrosantos@live.com>
2024-05-26 20:28:07 -0300

    Add moviments king and tower

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace tabuleiro {
-    class Peca {
+    abstract class Peca {
         public Posicao? posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
@@ -17,5 +17,7 @@ namespace tabuleiro {
         public void incrementarQtdMovimentos() {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
