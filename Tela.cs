8b8d48d6506aac49ee68b848bab7abaf749b0f1c
Lizandro Kenedy <lizandrosantos@live.com>
2024-05-26 15:33:25 -0300

    Initial movements pieces

--- Tela.cs
+++ Tela.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace xadrez_console {
     class Tela {
@@ -18,6 +19,13 @@ namespace xadrez_console {
             }
 
             Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez() {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca) {
